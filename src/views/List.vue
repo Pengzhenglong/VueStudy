3524620ee446cd1625d0591f128d4c84f371dae4
H.M.E <1316126217@qq.com>
2021-06-21 21:42:43 +0800

    增加动画效果

--- src/views/List.vue
+++ src/views/List.vue
@@ -16,17 +16,37 @@
       <ul>
         <li v-for="(tag, index) in tags" :key="index">
           <img :src="tag.image" alt="" />
-          <p>{{ tag.label }}<i  class="cubeic-add" @click="addtocart($event,tag)"></i></p>
+          <p>
+            {{ tag.label }}
+            <i class="cubeic-add" @click="addtocart($event, tag)"></i>
+          </p>
         </li>
       </ul>
     </cube-scroll>
+    <div class="ball-wrap">
+      <transition
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @afterEnter="afterEnter"
+      >
+        <div class="ball" v-if="ball.show">
+          <div class="inner">
+            <i class="cubeic-add"></i>
+          </div>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
-<script> 
+<script>
 export default {
   data() {
     return {
+      ball: {
+        show: false,
+        el: ''
+      },
       tags: [],
       tabslabel: [
         {
@@ -90,75 +110,131 @@ export default {
           val.active = false
         }
       })
-      this.getclasssify(index)
+      this.getclassify(index)
     },
-
-    // 获取分类
-    async getclasssify(index) {
+    //获取分类
+    async getclassify(index) {
       const result = await this.$http.get('/api/classify', { params: { type: index } })
       this.tags = result.data
     },
-    // 添加商品到购物车
-    addtocart(e,tag){
-          this.$store.commit('tocart',tag)
+    //添加商品到购物车
+    addtocart(e, tag) {
+      this.$store.commit('tocart', tag)
+      //让我们小球显示出来
+      this.ball.show = true
+      //获取点击元素
+      this.ball.el = e.target
+    },
+    beforeEnter(el) {
+      //让小球移动到点击的位置
+      //获取点击位置
+      const dom = this.ball.el
+      console.log(dom)
+      const rect = dom.getBoundingClientRect()//获取点击的dom的位置
+      console.log(rect)
+      const x = rect.left - window.innerWidth * 0.7
+      const y = -(window.innerHeight - rect.top)
+      console.log(x, y)
+      el.style.display = 'block'
+      el.style.transform = `translate3d(0,${y}px,0)`
+      const inner = el.querySelector('.inner')
+      inner.style.transform = `translate3d(${x}px,0,0)`
+    },
+    enter(el, done) {
+      //触发重绘
+      document.body.offsetHeight
+      //小球移动回到原点，就是购物车的位置
+      el.style.transform = `translate3d(0,0,0)`
+      const inner = el.querySelector('.inner')
+      inner.style.transform = `translate3d(0,0,0)`
+      //过渡完成后执行的事件
+      el.addEventListener('transitionend', done)
+    },
+    afterEnter(el) {
+      //结束隐藏小球
+      this.ball.show = false
+      el.style.display = 'none'
     }
   },
   created() {
-    // 获取默认的分类数据
-    this.getclasssify(0)
+    //获取默认的分类数据
+    this.getclassify(0)
   },
-  mounted(){
-    // 设置滚动盒子的高度
-      const  leftpanels = document.querySelector('.leftpanels')
-       const  rightpanels = document.querySelector('.rightpanels')
-       const  bodyheight  = document.documentElement.clientHeight
-       leftpanels.style.height=bodyheight-57+'px'
-        rightpanels.style.height=bodyheight-57+'px'
+  mounted() {
+    //设置滚动盒子的高度
+    const leftpanels = document.querySelector('.leftpanels')
+    const rightpanels = document.querySelector('.rightpanels')
+    const bodyheight = document.documentElement.clientHeight
+    leftpanels.style.height = bodyheight - 57 + 'px'
+    rightpanels.style.height = bodyheight - 57 + 'px'
   }
-
 }
 </script>
 
 <style lang="stylus" scoped>
-.panelsbox 
+.ball-wrap {
+  .ball {
+    position: fixed;
+    left: 70%;
+    bottom: 10px;
+    z-index: 1003;
+    color: red;
+    transition: all 1s cubic-bezier(0.49, -0.29, 0.75, 0.41);
+
+    .inner {
+      width: 16px;
+      height: 16px;
+      transition: all 1s linear;
+    }
+  }
+}
+
+.panelsbox {
   display: flex;
 
-  .leftpanels 
+  .leftpanels {
     width: 30%;
 
-    li 
+    li {
       height: 50px;
       line-height: 50px;
       border-bottom: 1px solid #fff;
       color: #333;
       background: #f8f8f8;
       font-size: 14px;
-    
+    }
 
-    .active 
+    .active {
       background: #fff;
       color: #e93b3d;
+    }
+  }
 
-  .rightpanels 
+  .rightpanels {
     width: 70%;
 
-    ul 
+    ul {
       display: flex;
       flex-wrap: wrap;
 
-      li 
+      li {
         width: 50%;
         justify-content: center;
         align-items: center;
         font-size: 15px;
 
-        img 
+        img {
           width: 80px;
           height: 80px;
-        
-        .cubeic-add
-          font-size:18px
-          margin-left :5px
+        }
 
-
+        .cubeic-add {
+          font-size: 18px;
+        }
+      }
+    }
+  }
+}
 </style>
+
+
